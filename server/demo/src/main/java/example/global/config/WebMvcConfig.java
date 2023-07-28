1f6bca14104fa5fc30989f3a2b11dcb3543c6935
jsujin13 <owjdtnwlso@naver.com>
2023-07-28 23:32:20 +0900

    Changes

--- server/demo/src/main/java/example/global/config/WebMvcConfig.java
+++ server/demo/src/main/java/example/global/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static java.awt.SystemColor.control;
+
 @Configuration // 스프링 빈으로 등록
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -12,6 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         // 모든 경로에 대해
         registry.addMapping("/**")
                 // Origin이 http:localhost:3000에 대해
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true);
     }
 }
