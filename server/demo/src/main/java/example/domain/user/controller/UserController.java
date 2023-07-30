49ee3d5f772a026d2d46f1f188fb09fbb75277d8
jsujin13 <owjdtnwlso@naver.com>
2023-07-30 23:43:48 +0900

    Changes

--- server/demo/src/main/java/example/domain/user/controller/UserController.java
+++ server/demo/src/main/java/example/domain/user/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.result.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
