42cc158ceb3daf8d257affcaddd02f3360ef5d41
jjaei <ieajnowk@gmail.com>
2023-08-11 00:53:08 +0900

    Changes

--- server/demo/src/main/java/example/global/config/WebMvcConfig.java
+++ server/demo/src/main/java/example/global/config/WebMvcConfig.java
@@ -1,19 +1,19 @@
-package example.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import static java.awt.SystemColor.control;
-
-@Configuration // 스프링 빈으로 등록
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 모든 경로에 대해
-        registry.addMapping("/**")
-                // Origin이 http:localhost:3000에 대해
-                .allowedOrigins("http://localhost:3000", "http://ec2-3-39-210-13.ap-northeast-2.compute.amazonaws.com:8080");
-    }
-}
+//package example.global.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import static java.awt.SystemColor.control;
+//
+//@Configuration // 스프링 빈으로 등록
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        // 모든 경로에 대해
+//        registry.addMapping("/**")
+//                // Origin이 http:localhost:3000에 대해
+//                .allowedOrigins("http://localhost:3000", "http://ec2-3-39-210-13.ap-northeast-2.compute.amazonaws.com:8080");
+//    }
+//}
