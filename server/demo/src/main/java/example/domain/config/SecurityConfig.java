b213cf8aefcff5608d78269bc1d8e3f3ac502b94
jjaei <ieajnowk@gmail.com>
2023-08-11 10:34:24 +0900

    Changes

--- server/demo/src/main/java/example/domain/config/SecurityConfig.java
+++ server/demo/src/main/java/example/domain/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
@@ -37,13 +38,15 @@ public class SecurityConfig {
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
-                .cors()
-                .and()
                 .sessionManagement().sessionCreationPolicy((SessionCreationPolicy.STATELESS)) // 세션 정책 설정
                 .and()
                 .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .anyRequest()
                 .permitAll()
+                .and()
+                .cors()
+                .configurationSource(corsConfigurationSource())
                 .and()
                 .oauth2Login() // OAuth 2 로그인 설정 진입점
                 .authorizationEndpoint()
