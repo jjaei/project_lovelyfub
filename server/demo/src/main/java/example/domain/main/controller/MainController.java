e6aba5828a7f71ebad542e2da72e3fe3b5a74c18
jsujin13 <owjdtnwlso@naver.com>
2023-09-03 21:10:57 +0900

    Changes

--- server/demo/src/main/java/example/domain/main/controller/MainController.java
+++ server/demo/src/main/java/example/domain/main/controller/MainController.java
@@ -1,12 +1,31 @@
 package example.domain.main.controller;
 
+import example.domain.store.entity.Store;
+import example.global.response.MultiResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@RestController
+@RequestMapping
+@Validated
+@Slf4j
 public class MainController {
-    @GetMapping("/main")
-    public void main(){
 
-    }
+        @GetMapping("/")
+        public ResponseEntity getMainPage() {
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
 }
