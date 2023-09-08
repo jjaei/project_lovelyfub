403037c9cf0918a1cd19d33914b1e8b56953177e
jsujin13 <owjdtnwlso@naver.com>
2023-09-09 01:55:09 +0900

    Changes

--- server/demo/build/generated/sources/annotationProcessor/java/main/example/domain/store/mapper/StoreMapperImpl.java
+++ server/demo/build/generated/sources/annotationProcessor/java/main/example/domain/store/mapper/StoreMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-09T01:21:49+0900",
+    date = "2023-09-09T01:38:05+0900",
     comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.1.1.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
 )
 @Component
