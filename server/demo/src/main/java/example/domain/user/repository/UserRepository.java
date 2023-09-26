411e169b364c117a922f6973d1578d24b6225725
jjaei <ieajnowk@gmail.com>
2023-09-26 17:02:01 +0900

    Feat: 러블리지수 Top10

--- server/demo/src/main/java/example/domain/user/repository/UserRepository.java
+++ server/demo/src/main/java/example/domain/user/repository/UserRepository.java
@@ -3,9 +3,12 @@ package example.domain.user.repository;
 import example.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
 	// email을 통해 이미 가입된 사용자인지 확인
+
+	List<User> findTop10ByOrderByLovelyCountDesc();
 }
