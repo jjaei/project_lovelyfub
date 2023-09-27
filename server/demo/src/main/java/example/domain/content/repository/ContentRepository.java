83b5b8319ce0f676eaca19ae1ce619c335c80fdf
jjaei <ieajnowk@gmail.com>
2023-09-27 15:19:57 +0900

    Feat: 마이페이지 구현(좋아요 한 가게, 내가 작성한 콘텐츠)

--- server/demo/src/main/java/example/domain/content/repository/ContentRepository.java
+++ server/demo/src/main/java/example/domain/content/repository/ContentRepository.java
@@ -3,5 +3,8 @@ package example.domain.content.repository;
 import example.domain.content.entity.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContentRepository extends JpaRepository<Content, Integer> {
+    List<Content> findByUserId(Long userId);
 }
--- server/demo/src/main/java/example/domain/user/controller/UserController.java
+++ server/demo/src/main/java/example/domain/user/controller/UserController.java
@@ -1,13 +1,8 @@
 package example.domain.user.controller;
 
-import example.domain.likes.entity.Likes;
-import example.domain.likes.service.LikesService;
-import example.domain.store.entity.Store;
-import example.domain.store.repository.StoreRepository;
 import example.domain.user.entity.User;
 import example.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,19 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class UserController {
 
-	private final StoreRepository storeRepository;
-	private final LikesService likesService;
 	private final UserRepository userRepository;
 
 	@GetMapping("/user")
@@ -55,49 +43,6 @@ public class UserController {
 		}
 	}
 
-	@GetMapping("/mypage")
-	public ResponseEntity<Map<String, Object>> myPage(Principal principal){
-
-		Map<String, Object> responseData = new HashMap<>();
-
-			String email = principal.getName();
-			Optional<User> optionalUser = userRepository.findByEmail(email);
-
-		if (optionalUser == null) {
-			// 로그인이 안 되어있는 상태면 로그인 페이지로
-			// 예를 들어 로그인 페이지의 URL이 /login 이라고 가정하면 아래와 같이 리다이렉트할 수 있음
-			return ResponseEntity.status(HttpStatus.FOUND)
-					.header("Location", "/login")
-					.build();
-		}
-
-		if (optionalUser.isPresent()) {
-			User user = optionalUser.get();
-			responseData.put("name", user.getName());
-			responseData.put("nickname", user.getNickname());
-			responseData.put("email", user.getEmail());
-			responseData.put("picture", user.getPicture());
-			responseData.put("sns_link", user.getSns_link());
-
-			List<Likes> likeStore = likesService.getAllLikes();
-			List<Store> likeStores = new ArrayList<>();
-
-			for (Likes likesStore : likeStore) {
-				Integer store_id = likesStore.getStore().getStoreid();
-				System.out.println(store_id);
-				Optional<Store> store = storeRepository.findByStoreid(store_id);
-
-				if (store.isPresent()) {
-					likeStores.add(store.get());
-				}
-			}
-			responseData.put("likeStores", likeStores);
-		}
-
-		// 200 OK 상태 코드와 responseData를 JSON 형태로 반환
-		return ResponseEntity.ok(responseData);
-	}
-
 	@PutMapping("/userInfo")
 	public ResponseEntity<?> loginHandler(@RequestBody User user, Principal principal) {
 
@@ -113,5 +58,4 @@ public class UserController {
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
 	}
-
 }
